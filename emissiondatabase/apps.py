eb1ffcba992a3d63474d4e234ba4fcc3d904fb7e
kiransaud <kiransaud49@gmail.com>
2024-06-14 15:21:49 +0300

    Clean up repository and ignore unnecessary files

--- emissiondatabase/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmissiondatabaseConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "emissiondatabase"
--- emissiondatabase/testserialize.py
+++
@@ -1,4 +0,0 @@
-from emissiondatabase.serializer import EmissionFactorSerializer
-
-em = EmissionFactorSerializer()
-print(em)
